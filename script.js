c69d44a2600d574c08fc102fdac8f6c8d0d3e40c
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-11-19 22:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    stopSounds();
+    const sound = new Audio(`sounds/${button.dataset.sound}.mp3`);
+    sound.play();
+    button.dataset.audio = sound; 
+  });
+});
+
+
+stopButton.addEventListener('click', stopSounds);
+
+function stopSounds() {
+  buttons.forEach(button => {
+    if (button.dataset.audio) {
+      button.dataset.audio.pause();
+      button.dataset.audio.currentTime = 0;
+    }
+  });
+}
